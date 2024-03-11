51edb33a15cba4e937ff5d80bc0f761e34d2c9fe
Margo <m.yunanova@gmail.com>
2024-03-11 23:18:16 +0300

    refactor: store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import groups from "../slices/groupsSlice";
 import { groupsApi } from "../utils/api";
 
 export const store = configureStore({
   reducer: {
-    groups,
     [groupsApi.reducerPath]: groupsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(groupsApi.middleware),
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-setupListeners(store.dispatch);
